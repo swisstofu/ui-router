fa64f08e1577b6699907b83a7a0f87a570efc062
Christophe Krebser <christophe.krebser@leshop.ch>
2016-01-27 22:11:21 +0100

    cleaning

--- test/urlMatcherFactorySpec.js
+++ test/urlMatcherFactorySpec.js
@@ -162,12 +162,12 @@ describe("UrlMatcher", function () {
       expect(m.exec('/', {})).toBeNull();
     });
 	
-	it("should work with empty default value", function () {
+    it("should work with empty default value", function () {
       var m = new UrlMatcher('/foo/:str', { params: { str: { value: "" } } });
       expect(m.exec('/foo/', {})).toEqual({ str: "" });
     });
 
-	it("should work with empty default value for regex", function () {
+    it("should work with empty default value for regex", function () {
       var m = new UrlMatcher('/foo/{param:(?:foo|bar|)}', { params: { param: { value: "" } } });
       expect(m.exec('/foo/', {})).toEqual({ param: "" });
     });
