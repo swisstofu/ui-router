37bcd502d4590ed3eb71693564dc0bdae675bb02
Christophe Krebser <christophe.krebser@leshop.ch>
2016-01-27 16:31:16 +0100

    release 0.2.17

--- release/angular-ui-router.js
+++ release/angular-ui-router.js
@@ -1,6 +1,6 @@
 /**
  * State-based routing for AngularJS
- * @version v0.2.17-ckr
+ * @version v0.2.17
  * @link http://angular-ui.github.com/
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
